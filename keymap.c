7fd4ea3833ccdec9c6a7323adae7ed760b80379b
Jezurko <63742204+Jezurko@users.noreply.github.com>
2022-07-19 14:48:29 +0200

    LOWER: move F keys up and add some special keys

--- keymap.c
+++ keymap.c
@@ -10,7 +10,16 @@ enum custom_keycodes {
     TILD_CZ,
     LESS_CZ,
     GRES_CZ,
-    HACEK
+    HACEK,
+    GRV_CZ,
+    CIRC_CZ,
+    LBRC_CZ,
+    AT_CZ,
+    HASH_CZ,
+    DOLA_CZ,
+    PERC_CZ,
+    AMPR_CZ
+
 };
 
 enum layer_number {
@@ -40,15 +49,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LCTRL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
                         KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   -  |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
@@ -59,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  _______, _______,   AT_CZ, HASH_CZ, DOLA_CZ, PERC_CZ,                   CIRC_CZ, AMPR_CZ,  LCBR_CZ, RCBR_CZ, LSBR_CZ, RSBR_CZ,
   KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR,  KC_LPRN, KC_RPRN, KC_TILD,
   _______, KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, KC_MPRV, KC_MPLY, KC_MNXT,/*half*/ _______, _______, KC_PIPE, LCBR_CZ, RCBR_CZ, LSBR_CZ, RSBR_CZ, _______,
                              KC_RALT, KC_MUTE, _______, _______, _______,  _______, KC_DEL, _______
@@ -83,8 +92,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_PPLS, KC_PMNS, KC_PSLS, KC_PAST, KC_PEQL, KC_PIPE,
-  _______, _______, _______, _______, _______, _______,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGUP, KC_PGDN,
-  _______, TILD_CZ, BSLS_CZ, PIPE_CZ, LESS_CZ, GRES_CZ,  _______, _______,  _______, _______, KC_EQL,  HACEK,   _______, _______,
+  _______, _______,   AT_CZ, HASH_CZ, DOLA_CZ, PERC_CZ,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGUP, KC_PGDN,
+  _______, TILD_CZ, BSLS_CZ, PIPE_CZ, LESS_CZ, GRES_CZ,  _______, LBRC_CZ,  GRV_CZ,  CIRC_CZ, KC_EQL,  HACEK,   _______, _______,
                              KC_RALT, _______, _______,  _______, _______,  _______, KC_DEL, KC_CAPS
 ),
 /* ADJUST
@@ -223,7 +232,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             unregister_code(KC_GRV);
             unregister_code(KC_LSFT);
             unregister_code(KC_RALT);
-        }
+       }
         break;
     case LESS_CZ:
         if (record->event.pressed) {
@@ -250,6 +259,78 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         } else {
             unregister_code(KC_EQL);
             unregister_code(KC_LSFT);
+        }
+        break;
+    case GRV_CZ:
+        if (record->event.pressed) {
+            register_code(KC_RALT);
+            register_code(KC_GRV);
+        } else {
+            unregister_code(KC_GRV);
+            unregister_code(KC_RALT);
+        }
+        break;
+    case CIRC_CZ:
+        if (record->event.pressed) {
+            register_code(KC_RALT);
+            register_code(KC_M);
+        } else {
+            unregister_code(KC_M);
+            unregister_code(KC_RALT);
+        }
+        break;
+    case LBRC_CZ:
+        if (record->event.pressed) {
+            register_code(KC_LSFT);
+            register_code(KC_RBRC);
+        } else {
+            unregister_code(KC_RBRC);
+            unregister_code(KC_LSFT);
+        }
+        break;
+    case AT_CZ:
+        if (record->event.pressed) {
+            register_code(KC_RALT);
+            register_code(KC_2);
+        } else {
+            unregister_code(KC_2);
+            unregister_code(KC_RALT);
+        }
+        break;
+    case HASH_CZ:
+        if (record->event.pressed) {
+            register_code(KC_RALT);
+            register_code(KC_3);
+        } else {
+            unregister_code(KC_3);
+            unregister_code(KC_RALT);
+        }
+        break;
+    case DOLA_CZ:
+        if (record->event.pressed) {
+            register_code(KC_RALT);
+            register_code(KC_4);
+        } else {
+            unregister_code(KC_4);
+            unregister_code(KC_RALT);
+        }
+        break;
+    case PERC_CZ:
+        if (record->event.pressed) {
+            register_code(KC_RALT);
+            register_code(KC_5);
+        } else {
+            unregister_code(KC_5);
+            unregister_code(KC_RALT);
+        }
+        break;
+    case AMPR_CZ:
+        if (record->event.pressed) {
+            register_code(KC_RALT);
+            register_code(KC_7);
+        } else {
+            unregister_code(KC_7);
+            unregister_code(KC_RALT);
         }
         break;
 
